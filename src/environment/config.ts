60adbcb458329ddbce2fdae8c453033da6b66faa
Wesley-M <wesleymatteus99@gmail.com>
2023-03-28 14:38:25 -0300

    Prod mode turned-on

--- src/environment/config.ts
+++ src/environment/config.ts
@@ -1,8 +1,9 @@
+type MODE_TYPE = 'prod' | 'dev';
 
 /**
  * Set the environment
 */
-const MODE = "dev";
+const MODE : MODE_TYPE = 'prod';
 
 /**
  * Page size for pagination purposes
@@ -13,10 +14,10 @@ const PAGE_SIZE = 10;
  * Get the API URL based on the environment
 */
 const getApiURL = () => {
-  if (MODE === "dev") {
+  if (MODE == 'dev' as MODE_TYPE) {
     return "http://192.168.0.102:5050/poems";
   } else {
-    return "https://api.example.com";
+    return "http://poetree-api.cyclic.app/poems";
   }
 }
 
